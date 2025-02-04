cb11614537c5bc379ce903bb03afa549f7d4994c
BastienDubois <bastien.dubois@student.uclouvain.be>
2025-02-04 11:21:48 +0100

    Recommandations init

---
+++ Recommandations.py
@@ -0,0 +1,28 @@
+# Objectif II
+
+"""
+Input: 
+airports.csv
+new_routes.csv
+waiting_times.csv
+prices.csv
+
+Ouput:
+path                array of edges to visit
+"""
+def recommandations(airports, new_routes, waiting_times, prices):
+    
+    return
+
+
+"""
+Input: 
+V                   array of vertices
+E                   array of edges
+values              array of edges' values
+start               starting airport
+end                 ending airport
+"""
+def dijkstra(V, E, values, start, end):
+
+    return
