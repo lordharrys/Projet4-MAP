d7e18b6d8bdef6bd1a11502536eaebabb1143474
alisia <alisia.fagnoul@student.uclouvain.be>
2025-02-07 16:40:52 +0100

    reco

--- Recommandations.py
+++ Recommandations.py
@@ -11,30 +11,29 @@ Ouput:
 path                array of edges to visit
 """
 def recommandations(airports, new_routes, waiting_times, prices):
-    
-    root = tk.Tk()
-    choix = tk.StringVar()
-    options = ["Distance", "Temps", "Prix"]
-    for option in options:
-        tk.Radiobutton(root, text=option, variable=choix, value=option).pack(anchor="w")
-    if choix.get() == "Distance":
+    preferences = input("Quel critère est le plus important pour vous ? (Distance, Temps, Prix) : ")
+    if preferences == "Distance":
         values = new_routes
-    if choix.get() == "Temps":
+    if preferences == "Temps":
         values = waiting_times
-    if choix.get() == "Prix":
+    if preferences == "Prix":
         values = prices
-        
-    start_choice = tk.Combobox(root, values=airports, state="readonly")
-    end_choice = tk.Combobox(root, values=airports, state="readonly")
-    # attention si aéroports pas connexes
-    start = start_choice.get()
-    end = end_choice.get()
-    if not start or not end:
-        tk.messagebox.showwarning("Erreur", "Veuillez sélectionner un aéroport de départ et d'arrivée.")
+    else:
+        print("Veuillez entrer un critère valide.")
         return
-    btn_valider = tk.Button(root, text="Meilleur trajet", command=dijkstra(V, E, values, start, end))
-    
-    root.mainloop()
+    start = input("Aéroport de départ : ")
+    if start not in airports:
+        print("Veuillez entrer un aéroport valide.")
+        return
+    end = input("Aéroport d'arrivée : ")
+    if end not in airports:
+        print("Veuillez entrer un aéroport valide.")
+        return
+    if dfs(start, end, new_routes)==False:
+        print("Ce trajet n'est pas possible.")
+        return
+    dijkstra(airports, new_routes, values, start, end)
+    return
 
 
 """
@@ -48,3 +47,17 @@ end                 ending airport
 def dijkstra(V, E, values, start, end):
 
     return
+
+def dfs(start, end, new_routes):
+    visited = set()
+    stack = [start]
+    while stack:
+        current = stack.pop()
+        if current == end:
+            return True
+        if current not in visited:
+            visited.add(current)
+            stack.extend(new_routes[current])
+    return False
+
+    
