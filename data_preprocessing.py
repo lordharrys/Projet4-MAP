db38fb1b39c8665c3dec71f837625cca866999a8
BastienDubois <bastien.dubois@student.uclouvain.be>
2025-02-11 10:14:14 +0100

    -

---
+++ data_preprocessing.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import networkx as nx
+from distance import distance
+
+
+def data_preprocessing(airports, routes):
+    """
+    """
+    airports = pd.read_csv(airports)
+    routes = pd.read_csv(routes)
+
+    G = nx.DiGraph()
+
+    for _, row in airports.iterrows():
+        G.add_node(row["ID"], name=row["name"], city=row["city"], country=row["country"], 
+                latitude=row["latitude"], longitude=row["longitude"])
+
+    for _, row in routes.iterrows():
+        start, end = row["ID_start"], row["ID_end"]
+        
+        if start in G.nodes and end in G.nodes:
+            lat1, lon1 = G.nodes[start]["latitude"], G.nodes[start]["longitude"]
+            lat2, lon2 = G.nodes[end]["latitude"], G.nodes[end]["longitude"]
+            
+            dist = distance(lat1, lon1, lat2, lon2)  
+            G.add_edge(start, end, weight=dist)
+    
+    return airports, routes, G
+
+
+airports, routes, G = data_preprocessing("files/airports.csv", "files/pre_existing_routes.csv")
---
+++ distance.py
@@ -0,0 +1,16 @@
+import math
+
+
+def distance(lat1, lat2, lon1, lon2):
+    """
+    """
+   
+    R = 6378137.0
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+    
+    distance = R * math.acos(math.sin(lat1_rad) * math.sin(lat2_rad) +
+                             math.cos(lat1_rad) * math.cos(lat2_rad) * math.cos(lon2_rad - lon1_rad))
+    return distance
