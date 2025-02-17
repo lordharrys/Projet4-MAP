8877ee7d11de7914db32fc2c4ffb57bf3c3b0a66
lordharrys <129772714+lordharrys@users.noreply.github.com>
2025-02-17 14:11:46 +0100

    Update BellmanFord.py

--- BellmanFord.py
+++ BellmanFord.py
@@ -1,11 +1,5 @@
 import matplotlib.pyplot as plt
 
-graph = {
-    1: {2: 4, 3: 2},
-    2: {3: 5, 4: 10},
-    3: {4: 3},
-    4: {}
-}
 
 edges = [(u, v, w) for u in graph for v, w in graph[u].items()]
 
@@ -28,13 +22,6 @@ def bellman_ford(graph, edges, start):
 
     return distances, parents
 
-source = 1
-try:
-    distances, parents = bellman_ford(graph, edges, source)
-except ValueError as e:
-    print(e)
-    exit()
-
 
 def reconstruct_path(parents, target):
     path = []
@@ -43,10 +30,5 @@ def reconstruct_path(parents, target):
         target = parents[target]
     return path[::-1]
 
-# Calcul des chemins
-paths = {node: reconstruct_path(parents, node) for node in graph if node != source}
-distances = {node: distances[node] for node in graph}
-
-print(distances)
 
 
