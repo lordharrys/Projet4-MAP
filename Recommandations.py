42e050d6d7e6dc51d73389dae1efb21e7c4e81d1
alisia <alisia.fagnoul@student.uclouvain.be>
2025-02-23 14:21:11 +0100

    interface finie

--- Recommandations.py
+++ Recommandations.py
@@ -11,66 +11,73 @@ prices.csv
 Ouput:
 path                array of edges to visit
 """
-def recommandations(G, waiting_times, prices,J,time):
-    preferences = input("Quel critère est le plus important pour vous ? (Distance, Temps, Prix) : ")
-    if preferences == "Distance":
-        values = nx.get_edge_attributes(G, "weight")
-    elif preferences == "Temps":
-        values = time
-    elif preferences == "Prix":
-        values = prices
-    else:
-        print("Veuillez entrer un critère valide.")
-        return
-    start = input("Aéroport de départ : ")
-    if start not in G.nodes:
-        print("Veuillez entrer un aéroport valide.")
-        return
-    end = input("Aéroport d'arrivée : ")
-    if end not in G.nodes:
-        print("Veuillez entrer un aéroport valide.")
-        return
-    if(start,end) not in J:
-        if dfs(start, end, G)==False:
-            print("Ce trajet n'est pas possible.")
-            return
-        
-    routes_values = nx.DiGraph(G)
-    if(preferences != "Distance"):
-        for u, v in routes_values.edges:
-            routes_values[u][v]["weight"] = values.get((u, v))
-            if preferences == "Temps":
-                if u != start :
-                    routes_values[u][v]["weight"] += waiting_times[u]
-    
-    solution = nx.shortest_path(G, source=start, target=end, weight="weight")
-    print("chemin trouvé yay")
-    print(solution)
-    return
-
 
 def recommandations_interface(G, waiting_times, prices,J,time):
-    window = tk.Tk()
-    window.title("Recommandations")
+    root = tk.Tk()
+    root.title("Recommandations")
     
-    Question1 = tk.Label(window, text="Quel critère est le plus important pour vous ? ")
+    preference = tk.StringVar()
+    preference.set("Distance")
+    
+    Question1 = tk.Label(root, text="Quel critère est le plus important pour vous ?")
     Question1.pack()
-    preference = tk.Scrollbar(window)
-    preference.pack(side=tk.RIGHT, fill=tk.Y)
-    listbox = tk.Listbox(window, yscrollcommand=preference.set)
-    listbox.insert(tk.END, "Distance")
-    listbox.insert(tk.END, "Temps")
-    listbox.insert(tk.END, "Prix")
-    listbox.pack()
-    preference.config(command=listbox.yview)
+    tk.Radiobutton(root, text="Distance", value="Distance", variable=preference).pack()
+    tk.Radiobutton(root, text="Temps", value="Temps", variable=preference).pack()
+    tk.Radiobutton(root, text="Prix", value="Prix", variable=preference).pack()
     
-    listbox.bind("<ButtonRelease-1>", lambda event: on_item_click(event, listbox))
     
-    window.mainloop()
+    start = tk.StringVar()
+    start.set("Aéroport de départ")
+    Question2 = tk.Label(root, text="Aéroport de départ : ")
+    Question2.pack()
+    for airport in G.nodes:
+        tk.Radiobutton(root, text=airport, value=airport, variable=start).pack()
+
     
-def on_item_click(event,listbox):
-    clicked_item = listbox.get(listbox.nearest(event.y))  # Get the item where the click occurred
-    return clicked_item
+    end = tk.StringVar()
+    end.set("Aéroport d'arrivée")
+    Question3 = tk.Label(root, text="Aéroport d'arrivée : ")
+    Question3.pack()
+    for airport in G.nodes:
+        tk.Radiobutton(root, text=airport, value=airport, variable=end).pack()
+
+    
+    Confirm3 = tk.Button(root, text="Confirmer le trajet", command=lambda: confirm_traject(root,start.get(),end.get(),G,J,preference.get(),prices,time,waiting_times))
+    Confirm3.pack()
+    
+    tk.mainloop()
+
+def confirm_traject(root,start,end,G,J,preference,prices,time,waiting_times):
+        if(start,end) not in J:
+            if dfs(start, end, G)==False:
+                label = tk.Label(root, text="Ce trajet n'est pas possible.")
+                label.pack()
+                return
+        label2 = tk.Label(root, text="Trajet confirmé.")
+        label2.pack()
+        
+        if preference == "Distance":
+            values = nx.get_edge_attributes(G, "weight")
+        elif preference == "Temps":
+            values = time
+        elif preference == "Prix":
+            values = prices
+        else:
+            label = tk.Label(root, text="Veuillez entrer un critère valide.")
+            label.pack()
+            return
+
+        routes_values = nx.DiGraph(G)
+        if preference != "Distance":
+            for u, v in routes_values.edges:
+                routes_values[u][v]["weight"] = values.get((u, v))
+                if preference == "Temps":
+                    if u != start:
+                        routes_values[u][v]["weight"] += waiting_times[u]
+
+        solution = nx.shortest_path(G, source=start, target=end, weight="weight")
+        label = tk.Label(root, text=f"Chemin trouvé: {solution}")
+        label.pack()
 
 
 def dfs(start, end, G):
