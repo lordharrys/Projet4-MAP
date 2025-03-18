670c5aa129f96eeada8ab66464b7e5fcb599ee91
alisia <alisia.fagnoul@student.uclouvain.be>
2025-03-18 10:42:00 +0100

    recommandations avec plot

--- Recommandations.py
+++ Recommandations.py
@@ -1,65 +1,133 @@
 # Objectif II
 import tkinter as tk
 import networkx as nx
-"""
-Input: 
-airports.csv
-new_routes.csv
-waiting_times.csv
-prices.csv
+import tkinter as tk
+import networkx as nx
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from data_processing import data_processing
 
-Ouput:
-path                array of edges to visit
-"""
 
-def recommandations_interface(G, waiting_times, prices,J,time):
+def plot_solution(G,solution):
+    """
+    Plots the network on a sphere representing the Earth by using cartopy.
+
+    Input:
+        - G: a networkx graph representing the network.
+    
+    """
+    fig = plt.figure(figsize=(12, 8))
+    ax = plt.axes(projection=ccrs.Orthographic(central_longitude=0, central_latitude=0))
+    ax.set_global()
+
+    ax.add_feature(cfeature.COASTLINE, linewidth=0.5)
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+
+    for node in G.nodes():
+        lat, lon = G.nodes[node]["latitude"], G.nodes[node]["longitude"]
+        ax.scatter(lon, lat, color='red', s=10, transform=ccrs.PlateCarree())
+
+    for i in range (len(solution)-1) :
+        start = solution[i]
+        end = solution[i+1]
+        lat1, lon1 = G.nodes[start]["latitude"], G.nodes[start]["longitude"]
+        lat2, lon2 = G.nodes[end]["latitude"], G.nodes[end]["longitude"]
+
+        ax.plot([lon1, lon2], [lat1, lat2], color='blue', linewidth=0.3, transform=ccrs.PlateCarree())
+
+    plt.title("Réseau aérien mondial avec Cartopy")
+    plt.show()
+
+
+def recommandations_interface(G, waiting_times, prices, J):
     root = tk.Tk()
     root.title("Recommandations")
+    
+    frame = tk.Frame(root)
+    frame.pack(fill="both", expand=True)
+    
+    canvas = tk.Canvas(frame)
+    canvas.pack(side="left", fill="both", expand=True)
+    
+    scrollbar = tk.Scrollbar(frame, orient="vertical", command=canvas.yview)
+    scrollbar.pack(side="right", fill="y")
+    
+    canvas.configure(yscrollcommand=scrollbar.set)
+    
+    # Create a frame inside the canvas to hold all the widgets
+    canvas_frame = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=canvas_frame, anchor="nw")
+
+    # Bind the scrollbar to the canvas's viewport
+    def on_frame_configure(event):
+        canvas.configure(scrollregion=canvas.bbox("all"))
+    
+    canvas_frame.bind("<Configure>", on_frame_configure)
     
     preference = tk.StringVar()
     preference.set("Distance")
     
-    Question1 = tk.Label(root, text="Quel critère est le plus important pour vous ?")
+    # Add question and radio buttons for preference
+    Question1 = tk.Label(canvas_frame, text="Quel critère est le plus important pour vous ?")
     Question1.pack()
-    tk.Radiobutton(root, text="Distance", value="Distance", variable=preference).pack()
-    tk.Radiobutton(root, text="Temps", value="Temps", variable=preference).pack()
-    tk.Radiobutton(root, text="Prix", value="Prix", variable=preference).pack()
-    
-    
+    tk.Radiobutton(canvas_frame, text="Distance", value="Distance", variable=preference).pack()
+    tk.Radiobutton(canvas_frame, text="Temps", value="Temps", variable=preference).pack()
+    tk.Radiobutton(canvas_frame, text="Prix", value="Prix", variable=preference).pack()
+
     start = tk.StringVar()
     start.set("Aéroport de départ")
-    Question2 = tk.Label(root, text="Aéroport de départ : ")
+    Question2 = tk.Label(canvas_frame, text="Aéroport de départ : ")
     Question2.pack()
     for airport in G.nodes:
-        tk.Radiobutton(root, text=airport, value=airport, variable=start).pack()
+        tk.Radiobutton(canvas_frame, text=airport, value=airport, variable=start).pack()
 
-    
     end = tk.StringVar()
     end.set("Aéroport d'arrivée")
-    Question3 = tk.Label(root, text="Aéroport d'arrivée : ")
+    Question3 = tk.Label(canvas_frame, text="Aéroport d'arrivée : ")
     Question3.pack()
     for airport in G.nodes:
-        tk.Radiobutton(root, text=airport, value=airport, variable=end).pack()
+        tk.Radiobutton(canvas_frame, text=airport, value=airport, variable=end).pack()
 
-    
-    Confirm3 = tk.Button(root, text="Confirmer le trajet", command=lambda: confirm_traject(root,start.get(),end.get(),G,J,preference.get(),prices,time,waiting_times))
+    # Add confirm button
+    Confirm3 = tk.Button(canvas_frame, text="Confirmer le trajet", command=lambda: confirm_traject(root, start.get(), end.get(), G, J, preference.get(), prices, waiting_times))
     Confirm3.pack()
     
-    tk.mainloop()
+    # List to store confirmed paths' labels
+    confirmed_labels = []
+    confirmed_limit = 3  # Limit the number of confirmed paths to 3
 
-def confirm_traject(root,start,end,G,J,preference,prices,time,waiting_times):
-        if(start,end) not in J:
-            if dfs(start, end, G)==False:
+    def confirm_traject(root, start, end, G, J, preference, prices, waiting_times):
+        if(start not in G.nodes or end not in G.nodes):
+            label = tk.Label(root, text="Veuillez entrer des aéroports valides.")
+            label.pack()
+            
+            if len(confirmed_labels) >= confirmed_limit:
+                label_to_remove = confirmed_labels.pop(0)  # Remove the first confirmed label
+                label_to_remove.destroy()  # Destroy the old label
+        
+        # Add the new label to the list of confirmed labels
+            confirmed_labels.append(label)
+            return
+        if (start, end) not in J:
+            if dfs(start, end, G) == False:
                 label = tk.Label(root, text="Ce trajet n'est pas possible.")
                 label.pack()
+                
+                if len(confirmed_labels) >= confirmed_limit:
+                    label_to_remove = confirmed_labels.pop(0)  # Remove the first confirmed label
+                    label_to_remove.destroy()  # Destroy the old label
+        
+        # Add the new label to the list of confirmed labels
+                confirmed_labels.append(label)
                 return
-        label2 = tk.Label(root, text="Trajet confirmé.")
-        label2.pack()
+        
+
         
         if preference == "Distance":
             values = nx.get_edge_attributes(G, "weight")
         elif preference == "Temps":
-            values = time
+            values = {k: v / 900 * 60 for k, v in nx.get_edge_attributes(G, "weight").items()} # on divise par la vitesse moyenne d'un avion et on multiplie par 60 pour avoir le temps en minutes
         elif preference == "Prix":
             values = prices
         else:
@@ -76,18 +144,31 @@ def confirm_traject(root,start,end,G,J,preference,prices,time,waiting_times):
                         routes_values[u][v]["weight"] += waiting_times[u]
 
         solution = nx.shortest_path(G, source=start, target=end, weight="weight")
-        label = tk.Label(root, text=f"Chemin trouvé: {solution}")
+        total_value = sum(routes_values[u][v]["weight"] for u, v in zip(solution[:-1], solution[1:]))
+    
+        label = tk.Label(root, text=f"Chemin trouvé: {solution}\nValeur totale: {total_value}")
         label.pack()
+        
+        plot_solution(G,solution)
+        
+        if len(confirmed_labels) >= confirmed_limit:
+            label_to_remove = confirmed_labels.pop(0)  # Remove the first confirmed label
+            label_to_remove.destroy()  # Destroy the old label
+        
+        # Add the new label to the list of confirmed labels
+        confirmed_labels.append(label)
 
+    def dfs(start, end, G):
+        visited = set()
+        stack = [start]
+        while stack:
+            current = stack.pop()
+            if current == end:
+                return True
+            if current not in visited:
+                visited.add(current)
+                stack.extend(neighbor for neighbor in G.neighbors(current) if neighbor not in visited)
+        return False
+    
+    tk.mainloop()
 
-def dfs(start, end, G):
-    visited = set()
-    stack = [start]
-    while stack:
-        current = stack.pop()
-        if current == end:
-            return True
-        if current not in visited:
-            visited.add(current)
-            stack.extend(neighbor for neighbor in G.neighbors(current) if neighbor not in visited)
-    return False
--- Recommandations_test.py
+++ Recommandations_test.py
@@ -1,17 +1,19 @@
 from Recommandations import recommandations_interface
 import networkx as nx
-from distance import distance
 import data_processing
 import Projet4
+import pandas as pd
+import distance
 
 G, edges = data_processing.data_processing("files/airports.csv", "files/pre_existing_routes.csv")
 
 
-# Create a small graph with 5 airports
-G = nx.DiGraph()
+attente = pd.read_csv("files/waiting_times.csv")
+prix = pd.read_csv("files/prices.csv")
 
-pairs_to_connect = [('LOS', 'BOS'),('YYZ','BOS'),('JFK','YYZ'),('AKL','LOS'),('AKL','LAX'),('IAH','LAX'),('IAH','JFK')]
-model = Projet4.resolution(G, pairs_to_connect, edges, 0)
+prices = dict(zip(zip(prix["ID_start"], prix["ID_end"]), prix["price_tag"]))
+waiting_times = dict(zip(attente["ID"], attente["idle_time"]))
+
 
 # Test the recommandations function
-recommandations_interface(G, waiting_times, prices,J,time)
+recommandations_interface(G, waiting_times, prices,[('BKK','ADD')])
